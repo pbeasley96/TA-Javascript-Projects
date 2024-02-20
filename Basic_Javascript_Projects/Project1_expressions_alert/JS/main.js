1f5dcb09d3a6549be947abb9871fc6153c07fd26
pbeasley96 <156345819+pbeasley96@users.noreply.github.com>
2024-02-20 08:18:22 -0600

    Basic_Javascript_Project
    Created a HTML file and utilized Basic Javascript functions

---
+++ Basic_Javascript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,36 @@
+alert("Hello World! How are you today?")     //Created an alert window//
+
+Sent1 = "This is the beginning of the string,";     //Created two variables each with a string value//
+Sent2 = "This is the end of the string.";
+
+document.write(Sent1 + Sent2);     //Used the + operator to concatenate the two values together//
+
+document.write("<br>")     //Used the break line tag to create space between inputs//
+document.write("<br>")
+
+var B = "My favorite anime is \"Yu Yu Hakusho,\" written by Yoshihiro Togashi";    //Wrote a string that contains characters for escape//
+
+document.write(B);
+
+document.write("<br>")
+document.write("<br>")
+
+var C = ("\"We keep moving forward,"     // Used the + operator to concatenate a string of text//
+    + "opening new doors, and doing new things,"
+    + "because we\'re curious and curiousity keeps leading us down new paths.\""
+    + "- Walt Disney");
+
+document.write(C);
+
+document.write("<br>")
+document.write("<br>")
+
+var Family = "The Son Family", Dad = "Goku", Mom = "Chi-Chi", FirstSon = "Gohan"     //Assigned multiple variables in on statement//
+    SecondSon = "Goten";
+
+document.write(FirstSon);   //Used the document.write() method to display one of the variables//
+
+document.write("<br>")
+document.write("<br>")
+
+document.write(4+4);     //Used the document.write() method to write an expression.//
