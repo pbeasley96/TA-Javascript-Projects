ea9581d5b2946e67035d38a8dc70cb178e44597d
pbeasley96 <156345819+pbeasley96@users.noreply.github.com>
2024-02-26 10:42:55 -0600

    Project9_countdown_slideshow
    Created a countdown() function and     Created a basic slideshow using HTML, CSS, and JavaScript

---
+++ Basic_Javascript_Projects/Project9_countdown_slideshow/JS/main.js
@@ -0,0 +1,30 @@
+function countdown() {          //Create a countdown() function.//
+    var seconds = document.getElementById("seconds").value;
+
+    function tick() {
+        seconds = seconds - 1;
+        timer.innerHTML = seconds;
+        var time = setTimeout(tick, 1000);
+        if (seconds == -1) {
+            alert("Time's up!");
+            clearTimeout(time);
+            timer.innerHTML = "";
+        }
+    }
+    tick();
+}
+
+let slideIndex = 0;          //Using reference I created a function that created a slideshow that runs automatically.//
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 5 seconds.//
+} 
